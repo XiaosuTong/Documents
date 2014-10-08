275bc70dd49e510e157f542084e89124720ff6b6
XiaosuTong <xiaosutong@gmail.com>
2014-10-08 19:40:33 -0400

    update Python docs

--- Python/Chapter1/1.14.py
+++ Python/Chapter1/1.14.py
@@ -2,7 +2,7 @@
 class User:
 	def __init__(self, user_id):
 		self.user_id = user_id
-	def __repr__(self):
+	def __repr__(self): #how the class be represented
 		return 'User({})'.format(self.user_id)
 
 users = [User(23), User(4), User(99)]
